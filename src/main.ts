8c49e14a77487259d5e32434d50a0e82c7443034
1920ben <1920ben@users.noreply.github.com>
2025-10-28 09:29:25 -0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,54 +1,121 @@
-import { Plugin } from "obsidian";
-import { JournalingSettingTab } from "./settings";
-import "virtual:uno.css";
-import journalingView from "./scripts/JournalingView";
+import { App, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
+
+// Remember to rename these classes and interfaces!
 
 interface JournalingPluginSettings {
-    dateFormat: string;
-    paths: string;
-    fileName: string;
-    filterValue: string;
-    updateInterval: number;
+	journalName: string;
+	locale: string;
 }
 
-const DEFAULT_SETTINGS: Partial<JournalingPluginSettings> = {
-    dateFormat: "YYYY-MM-DD",
-    paths: "",
-    fileName: "Journaling.md",
-    filterValue: "new",
-    updateInterval: 15,
-};
+const DEFAULT_SETTINGS: JournalingPluginSettings = {
+	journalName: 'Journal',
+	locale: 'en-US'
+}
+
+async function createJournalEntry(app: App, settings: JournalingPluginSettings) {
+	const now = new Date();
+	const date = now.toLocaleDateString(settings.locale, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+	const time = now.toLocaleTimeString(settings.locale, { hour: '2-digit', minute: '2-digit' });
+
+	// MODIFICATION: Changed from bold (**) to a Level 2 Heading (##) to make it larger.
+	// You can change ## to # for an even larger heading, or ### for a smaller one.
+	const entry = `\n## ${date} ${time}\n\n`;
+
+	const fileName = `${settings.journalName}.md`;
+
+	let file = app.vault.getAbstractFileByPath(fileName);
+	if (!file) {
+		file = await app.vault.create(fileName, "");
+	}
+
+	if (file instanceof TFile) {
+		await app.vault.append(file, entry);
+		const leaf = app.workspace.getLeaf(true);
+		await leaf.openFile(file);
+
+		// Move cursor to the end of the file
+		const editor = app.workspace.activeEditor?.editor;
+		if (editor) {
+			const length = editor.lastLine();
+			editor.setCursor({ line: length + 1, ch: 0 });
+		}
+	}
+}
 
 export default class JournalingPlugin extends Plugin {
-    settings!: JournalingPluginSettings;
-    private intervalId: number | null = null;
+	settings: JournalingPluginSettings;
 
-    async loadSettings() {
-        this.settings = Object.assign(
-            {},
-            DEFAULT_SETTINGS,
-            await this.loadData(),
-        );
-    }
+	async onload() {
+		await this.loadSettings();
 
-    async saveSettings() {
-        await this.saveData(this.settings);
+		// This creates an icon in the left ribbon.
+		const ribbonIconEl = this.addRibbonIcon('calendar-plus', 'Create journal entry', (evt: MouseEvent) => {
+			// Called when the user clicks the icon.
+			createJournalEntry(this.app, this.settings);
+		});
+		// Perform additional things with the ribbon
+		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-        if (this.intervalId) window.clearInterval(this.intervalId);
-        this.intervalId = await journalingView(this.app, this);
-    }
+		// This adds a simple command that can be triggered anywhere
+		this.addCommand({
+			id: 'create-journal-entry',
+			name: 'Create journal entry',
+			callback: () => {
+				createJournalEntry(this.app, this.settings);
+			}
+		});
 
-    async onload() {
-        await this.loadSettings();
 
-        this.addSettingTab(new JournalingSettingTab(this.app, this));
+		// This adds a settings tab so the user can configure various aspects of the plugin
+		this.addSettingTab(new JournalingSettingTab(this.app, this));
+	}
 
-        if (this.intervalId) window.clearInterval(this.intervalId);
-        this.intervalId = await journalingView(this.app, this);
-    }
+	onunload() {
 
-    onunload() {
-        if (this.intervalId) window.clearInterval(this.intervalId);
-        console.log("unloading plugin");
-    }
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
 }
+
+
+class JournalingSettingTab extends PluginSettingTab {
+	plugin: JournalingPlugin;
+
+	constructor(app: App, plugin: JournalingPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const { containerEl } = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Journal name')
+			.setDesc('The name of the journal file (without .md)')
+			.addText(text => text
+				.setPlaceholder('Enter your journal name')
+				.setValue(this.plugin.settings.journalName)
+				.onChange(async (value) => {
+					this.plugin.settings.journalName = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Locale')
+			.setDesc('The locale to use for date and time formatting (e.g. en-US, en-GB, de-DE)')
+			.addText(text => text
+				.setPlaceholder('Enter your locale')
+				.setValue(this.plugin.settings.locale)
+				.onChange(async (value) => {
+					this.plugin.settings.locale = value;
+					await this.plugin.saveSettings();
+				}));
+	}
